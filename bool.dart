4224afb4b30dab6d5d7b91cd08ad11a873852897
Boscus Sililo <boscosilizz4@gmail.com>
2023-03-15 14:39:58 -0800

    first commit

---
+++ bool.dart
@@ -0,0 +1,10 @@
+//Testing For Equality (Control Flows)
+void main() {
+   bool doesOneEqualTwo = (2==2);
+  print(doesOneEqualTwo);
+
+  //Testing For InEquality
+  const doesOneNotEqualTwo = (1!=2);
+  print(doesOneNotEqualTwo);
+
+}
